a1a7d6be5c1b0d2726f1ad9f2eebf83c8b28c739
Tom LJ <34989869+tj2211tj@users.noreply.github.com>
2019-10-02 10:44:43 +0100

    Changed to correct submission files

--- plot3.R
+++ plot3.R
@@ -7,4 +7,5 @@ plot(dat$DateTime,dat$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering"
 lines(dat$DateTime,dat$Sub_metering_1)
 lines(dat$DateTime,dat$Sub_metering_2,col="red")
 lines(dat$DateTime,dat$Sub_metering_3,col="blue")
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"),lty = 1:1, cex = 0.8)
 dev.off()
--- plot4.R
+++ plot4.R
@@ -2,8 +2,8 @@ dat<-read.table("household_power_consumption.txt",sep=";",header=TRUE,stringsAsF
 dat<-dat[dat$Date=="1/2/2007" | dat$Date=="2/2/2007",]
 dat$DateTime<-strptime(paste(dat$Date,dat$Time,sep=" ",collapse=NULL),format="%d/%m/%Y %H:%M:%S")
 
-par(mfrow=c(2,2))
 png(filename = "plot4.png",width = 480,height = 480)
+par(mfrow=c(2,2))
 plot(dat$DateTime,dat$Global_active_power,type="n",xlab="",ylab="Global Active Power (kilowatts)")
 lines(dat$DateTime,dat$Global_active_power)
 plot(dat$DateTime,dat$Voltage,type="n",xlab="datetime",ylab="Voltage")
@@ -12,6 +12,7 @@ plot(dat$DateTime,dat$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering"
 lines(dat$DateTime,dat$Sub_metering_1)
 lines(dat$DateTime,dat$Sub_metering_2,col="red")
 lines(dat$DateTime,dat$Sub_metering_3,col="blue")
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"),lty = 1:1, cex = 0.8)
 plot(dat$DateTime,dat$Global_reactive_power,type="n", xlab="datetime",ylab="Global_reactive_power")
 lines(dat$DateTime,dat$Global_reactive_power)
 dev.off()
